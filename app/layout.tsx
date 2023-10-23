d0ac0b948121248e37c3a286aa5ef65ce18b2862
Safo Emmanuel <nanasafo122@gmail.com>
2023-10-23 08:36:29 +0000

    fourth commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ToastProvider } from '@/components/providers/toaster-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToastProvider/>
           {children}
         </body>
       </html>
