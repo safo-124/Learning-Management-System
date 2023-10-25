ef855d148f231013e1bed1c85de23f92627b7777
Safo Emmanuel <nanasafo122@gmail.com>
2023-10-25 14:24:48 +0000

    thirteenth commit

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,6 +9,7 @@ import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
+import { ChaptersForm } from "./_components/chapters-form";
 
 const CourseIdPage = async ({
     params
@@ -22,9 +23,15 @@ const CourseIdPage = async ({
 
     const course = await db.course.findUnique({
         where: {
-            id: params.courseId
+            id: params.courseId,
+            userId
         },
         include: {
+            chapters: {
+                orderBy: {
+                    position: "asc"
+                },
+            },
             attachments: {
                 orderBy: {
                     createdAt: "desc"
@@ -49,6 +56,7 @@ const CourseIdPage = async ({
         course.imageUrl,
         course.price,
         course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished),
     ];
     const totalFields = requiredFields.length;
     const completedFields = requiredFields.filter(Boolean).length;
@@ -103,9 +111,10 @@ const CourseIdPage = async ({
                                     Course chapters
                                 </h2>
                             </div>
-                            <div>
-                                TODO: Chapters
-                            </div>
+                            <ChaptersForm
+                                initialData={course}
+                                courseId={course.id}
+                            />
                         </div>
                         <div>
                             <div className="flex items-center gap-x-2">
